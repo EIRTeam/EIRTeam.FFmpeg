b41b3c5cf3cc54f5899f4bf788fa9273af2fd981
Setadokalo <darkenchanter1@gmail.com>
2023-12-02 22:50:22 -0600

    Fix resource loader not informing singleton what extensions it supports

--- video_stream_ffmpeg_loader.h
+++ video_stream_ffmpeg_loader.h
@@ -64,6 +64,7 @@ public:
 	STREAM_FUNC_REDIRECT_1_CONST(String, get_resource_type, const String &, p_path);
 #ifdef GDEXTENSION
 	virtual PackedStringArray _get_recognized_extensions() const override;
+	virtual bool _handles_type(const StringName &p_type) const override;
 	virtual Variant _load(const String &p_path, const String &p_original_path, bool p_use_sub_threads, int32_t p_cache_mode) const override;
 #else
 	virtual void get_recognized_extensions(List<String> *p_extensions) const override;
